c0f6ef0da70aefeff984d6d1d5a6d72aa4a9a81e
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-02-23 12:51:45 +0530

    Add ApiService and HttpClient imports and implement getData method***
    ***Create ApiService unit test***
    
    ***Update AppComponent to use ApiService and HttpClient to fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'aquera';
+  data: any;
+
+  constructor(private apiService : ApiService, private httpClient : HttpClient) { }
+
+  ngOnInit() {
+    this.apiService.getData().subscribe((data: any) => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 }
